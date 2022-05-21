f2453e04227bb97f9923a5cedbbaf54f4659e6a5
Vitória Souza <viihsouza161@gmail.com>
2022-05-21 20:13:14 -0300

    DTMoney: added functionality of listing new transaction

--- chapter-II/dtmoney/src/components/TransactionsTable/TransactionsTable.tsx
+++ chapter-II/dtmoney/src/components/TransactionsTable/TransactionsTable.tsx
@@ -1,11 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number
+  title: string
+  amount: number
+  type: "deposit" | "withdraw"
+  category: string
+  createdAt: string
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
 
   useEffect(() => {
-    api.get("/transactions").then(response => console.log(response.data))
+    api.get("/transactions").then(response => setTransactions(response.data.transactions))
   }, [])
 
   return (
@@ -20,18 +30,16 @@ export function TransactionsTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento One page</td>
-            <td className="deposit">R$ 4.000,00</td>
-            <td>Freelancer</td>
-            <td>25/05/2022</td>
-          </tr>
-          <tr>
-            <td>Conta de Luz</td>
-            <td className="withdraw">- R$ 100,00</td>
-            <td>Casa</td>
-            <td>10/05/2022</td>
-          </tr>
+          {
+            transactions.map(transaction => (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>R$ {transaction.amount}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            ))
+          }
         </tbody>
       </table>
     </Container>
--- chapter-II/dtmoney/src/index.tsx
+++ chapter-II/dtmoney/src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transactions: Model
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          createdAt: new Date("2022-03-05 09:00:00")
+        },
+        {
+          id: 2,
+          title: "Transaction 2",
+          amount: 100,
+          type: "withdraw",
+          category: "Food",
+          createdAt: new Date("2022-03-07 21:00:00")
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = "api";
     this.get("/transactions", () => {

128211c7b33b7d89a861103576e1c590ab9c7b3f
Vitória Souza <viihsouza161@gmail.com>
2022-05-21 16:42:48 -0300

    DTMoney: added Dashboard and Summary components

---
+++ chapter-II/dtmoney/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+import { Summary } from "../Summary/Summary";
+
+export default function Dashboard() {
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  )
+}

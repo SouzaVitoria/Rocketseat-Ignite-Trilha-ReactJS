d8d0ce4c16155d7ec9adb97d15797e12577cff4f
Vitória Souza <viihsouza161@gmail.com>
2022-05-21 16:54:35 -0300

    DTMoney: added Transactions Table component

--- chapter-II/dtmoney/src/components/Dashboard/Dashboard.tsx
+++ chapter-II/dtmoney/src/components/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import { Container } from "./styles";
 import { Summary } from "../Summary/Summary";
+import { TransactionsTable } from "../TransactionsTable/TransactionsTable";
 
 export default function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   )
 }
---
+++ chapter-II/dtmoney/src/components/TransactionsTable/TransactionsTable.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento One page</td>
+            <td className="deposit">R$ 4.000,00</td>
+            <td>Freelancer</td>
+            <td>25/05/2022</td>
+          </tr>
+          <tr>
+            <td>Conta de Luz</td>
+            <td className="withdraw">- R$ 100,00</td>
+            <td>Casa</td>
+            <td>10/05/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
